f7ffcde5e0a4b269c7aa09802bf402cc524f079f
kelingg <kelingg@jumei.com>
2022-04-01 16:57:05 +0800

    add maxDrawdown func

---
+++ example/test_financial.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guokeling
+ * Date: 2018/12/10
+ * Time: 19:10
+ */
+
+require_once __DIR__ . '/../Autoload.php';
+
+$sortItems = array();
+$sortItems['20220101'] = array('market_value' => 100);
+$sortItems['20220102'] = array('market_value' => 200);
+$sortItems['20220103'] = array('market_value' => 150);
+$sortItems['20220104'] = array('market_value' => 320);
+$sortItems['20220105'] = array('market_value' => 400);
+$sortItems['20220106'] = array('market_value' => 260);
+$sortItems['20220107'] = array('market_value' => 500);
+$sortItems['20220108'] = array('market_value' => 150);
+$sortItems['20220109'] = array('market_value' => 160);
+$sortItems['20220110'] = array('market_value' => 200);
+$sortItems['20220111'] = array('market_value' => 500);
+$sortItems['20220112'] = array('market_value' => 320);
+$sortItems['20220113'] = array('market_value' => 460);
+$sortItems['20220114'] = array('market_value' => 550);
+$sortItems['20220115'] = array('market_value' => 600);
+
+try {
+    foreach($sortItems as $day => $item){
+        print $day;
+        print "\t";
+        print $item['market_value'];
+        print "\n";
+    }
+    $result = \PHPHelper\helpers\FinancialHelper::getMaxDrawdown($sortItems);
+    print_r($result);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
---
+++ helpers/FinancialHelper.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wind
+ * Date: 2022/4/1
+ * Time: 15:36
+ */
+
+namespace PHPHelper\helpers;
+
+
+class FinancialHelper
+{
+
+    /**
+     * 计算最大回撤, 并返回最大回撤和区间
+     *
+     * @param $sortItems array("20220101"=>array("market_value" => 101.20),……,"20220105"=>array("market_value" => 150.50))
+     * @return array
+     */
+    public static function getMaxDrawdown($sortItems)
+    {
+
+        $days = array();
+        $marketValues = array();
+        // 遍历取出日期和每天市值列表
+        foreach ($sortItems as $day => $item) {
+            $days[] = $day;
+            $marketValues[] = $item['market_value'];
+        }
+
+        $minMarketValues = array();
+        $minDates = array();
+        $tempMinMarketValue = null;
+        $minDate = null;
+
+        // 从后往前遍历市值列表, 计算每一天在当天之后的最小市值.
+        for ($i = -1; $i < -count($marketValues) - 1; $i--) {
+            if (is_null($tempMinMarketValue) or $marketValues[$i] < $tempMinMarketValue) {
+                $tempMinMarketValue = $marketValues[$i];
+                $minDate = $days[$i];
+            }
+            $minMarketValues[] = $tempMinMarketValue;
+            $minDates[] = $minDate;
+
+        }
+        $minMarketValues = array_reverse($minMarketValues);
+        $minDates = array_reverse($minDates);
+
+        $maxDrawdownValue = 0;
+        $region = array();
+        $i = 0;
+        // 从前往后遍历, 计算当天买入在之后的最大回撤, 然后得出区间内最大回撤.
+        foreach ($marketValues as $marketValue) {
+            $drawdown = ($marketValue - $minMarketValues[$i]) / $minMarketValues[$i];
+            if ($maxDrawdownValue < $drawdown) {
+                $maxDrawdownValue = $drawdown;
+                $region = array(array($days[$i], round($marketValue, 2)), array($minDates[$i], round($minMarketValues[$i], 2)));
+            }
+        }
+
+        return array($maxDrawdownValue, $region);
+
+    }
+
+}
