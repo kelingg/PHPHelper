6e7ebca5000ad20fd43e7bd0bb1aa430b8f87e17
kelingg <kelingg@jumei.com>
2022-04-01 17:53:58 +0800

    fix

--- example/test_financial.php
+++ example/test_financial.php
@@ -9,8 +9,8 @@
 require_once __DIR__ . '/../Autoload.php';
 
 $sortItems = array();
-$sortItems['20220101'] = array('market_value' => 100);
-$sortItems['20220102'] = array('market_value' => 200);
+$sortItems['20220101'] = array('market_value' => 650);
+$sortItems['20220102'] = array('market_value' => 1000);
 $sortItems['20220103'] = array('market_value' => 150);
 $sortItems['20220104'] = array('market_value' => 320);
 $sortItems['20220105'] = array('market_value' => 400);
@@ -19,11 +19,11 @@ $sortItems['20220107'] = array('market_value' => 500);
 $sortItems['20220108'] = array('market_value' => 150);
 $sortItems['20220109'] = array('market_value' => 160);
 $sortItems['20220110'] = array('market_value' => 200);
-$sortItems['20220111'] = array('market_value' => 500);
+$sortItems['20220111'] = array('market_value' => 550);
 $sortItems['20220112'] = array('market_value' => 320);
 $sortItems['20220113'] = array('market_value' => 460);
-$sortItems['20220114'] = array('market_value' => 550);
-$sortItems['20220115'] = array('market_value' => 600);
+$sortItems['20220114'] = array('market_value' => 50);
+$sortItems['20220115'] = array('market_value' => 280);
 
 try {
     foreach($sortItems as $day => $item){
--- helpers/FinancialHelper.php
+++ helpers/FinancialHelper.php
@@ -34,11 +34,15 @@ class FinancialHelper
         $tempMinMarketValue = null;
         $minDate = null;
 
+        $marketValueCount = count($marketValues);
         // 从后往前遍历市值列表, 计算每一天在当天之后的最小市值.
-        for ($i = -1; $i < -count($marketValues) - 1; $i--) {
-            if (is_null($tempMinMarketValue) or $marketValues[$i] < $tempMinMarketValue) {
-                $tempMinMarketValue = $marketValues[$i];
-                $minDate = $days[$i];
+        for ($i = -1; $i > -$marketValueCount - 1; $i--) {
+            $index = $marketValueCount + $i;
+            $currentValue = $marketValues[$index];
+            $currentDay = $days[$index];
+            if (is_null($tempMinMarketValue) or $currentValue < $tempMinMarketValue) {
+                $tempMinMarketValue = $currentValue;
+                $minDate = $currentDay;
             }
             $minMarketValues[] = $tempMinMarketValue;
             $minDates[] = $minDate;
@@ -46,21 +50,20 @@ class FinancialHelper
         }
         $minMarketValues = array_reverse($minMarketValues);
         $minDates = array_reverse($minDates);
-
         $maxDrawdownValue = 0;
         $region = array();
         $i = 0;
         // 从前往后遍历, 计算当天买入在之后的最大回撤, 然后得出区间内最大回撤.
         foreach ($marketValues as $marketValue) {
-            $drawdown = ($marketValue - $minMarketValues[$i]) / $minMarketValues[$i];
+            $drawdown = ($marketValue - $minMarketValues[$i]) / $marketValue;
             if ($maxDrawdownValue < $drawdown) {
                 $maxDrawdownValue = $drawdown;
                 $region = array(array($days[$i], round($marketValue, 2)), array($minDates[$i], round($minMarketValues[$i], 2)));
             }
+            $i++;
         }
 
         return array($maxDrawdownValue, $region);
 
     }
-
 }
